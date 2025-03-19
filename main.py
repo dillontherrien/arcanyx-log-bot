4af695c341fce16b42192b1914f4f2ed7fa1c5c9
Dillon <dillontherrien5@gmail.com>
2025-03-18 20:17:15 -0400

    mongodb used. payout and donation working

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import asyncio
 import os
 import re
+from datetime import datetime, timezone
 from dotenv import load_dotenv
-from interactions import Client, Intents, SlashContext, listen, slash_command, slash_option, OptionType
+from interactions import Client, Intents, Member, SlashContext, listen, slash_command, slash_option, OptionType
+from pymongo import MongoClient
 
 # Loads environment variables
 load_dotenv()
 
-bot = Client(intents=Intents.DEFAULT)
-
+# Functions
 def parse_amount(amount: str) -> int:
     """
     Takes the valid input and multiplys it based on letter if present.
@@ -21,15 +21,57 @@ def parse_amount(amount: str) -> int:
     
     return int(amount)
 
-@listen()
-async def on_ready():
-    print("Ready")
-    print(f"This bot is owned by {bot.owner}")
+def get_mongo_client() -> MongoClient:
+    """
+    Creates a MonboDB client.
+    """
+    mongo_url = os.getenv("MONGO_URL")
+    if not mongo_url:
+        raise ValueError("MONGO_URL environment variable is not set.")
+    
+    client = MongoClient(mongo_url)
+    return client
 
-# Basic command
-# @slash_command(name="my_command", description="My first command :)")
-# async def my_command_function(ctx: SlashContext):
-#     await ctx.send("Hello World")
+# Initializes the database
+mongo_db = get_mongo_client()
+# Selects the correct database
+db = mongo_db["arcanyx"]
+
+
+def insert_transaction(type, discord_id, staff_discord_id, amount):
+     # Ensure discord_id is a string or a primitive type
+    if isinstance(discord_id, Member):
+        discord_id = discord_id.id  # Extract the discordId from the Member object
+
+    if isinstance(staff_discord_id, Member):
+        staff_discord_id = staff_discord_id.id  # Extract the staff discordId from the Member object
+
+    # Find the member_id based on discordId
+    member = db.members.find_one({"discordId": str(discord_id)})
+    staff = db.members.find_one({"discordId": str(staff_discord_id)})
+
+    if not type:
+        print(f"Transaction type not defined!")
+        return
+    if not member:
+        print(f"Member with discordId {discord_id} not found!")
+        return
+    if not staff:
+        print(f"Staff member with discordId {staff_discord_id} not found!")
+        return
+
+    # Create transaction document
+    transaction = {
+        "type": type,
+        "member_id": member["_id"],
+        "staff_id": staff["_id"],
+        "amount": amount,
+        "donation_time": datetime.now(timezone.utc)
+    }
+
+    # Insert into transaction_log
+    result = db.transaction_log.insert_one(transaction)
+    print(f"Transaction inserted with _id: {result.inserted_id}")
 
 # Regex pattern to ensure OSRS gp logic is enforced
 AMOUNT_PATTERN = r"^\d*[kmbKMB]?$"
@@ -48,16 +90,72 @@ AMOUNT_PATTERN = r"^\d*[kmbKMB]?$"
     required=True,
     opt_type=OptionType.STRING
 )
-async def my_command_function(ctx: SlashContext, user: str, amount: str):
+async def donation_command(ctx: SlashContext, user: str, amount: str):
+    await ctx.defer()
+    amount_str = amount
     amount = amount.replace(",", "")
     
     # Checks that the input amount is valid format
     if not re.match(AMOUNT_PATTERN, amount):
         await ctx.send(f"Amount given ({amount}) is not valid. Please follow same logic in game for typing amounts (eg. 42244, 24m, 11k)")
         return
-    
-    amount = parse_amount(amount)
-        
-    await ctx.send(f"You input {amount}")
 
+    type = "donation"
+    amount = parse_amount(amount)
+    staff = ctx.author_id
+
+    insert_transaction(type, user, staff, amount)# Fetch the staff member's username for mention
+    
+    staff_member = await ctx.bot.fetch_user(staff)
+    staff_mention = f"<@{staff_member.id}>"  # Mention staff
+
+    # Send confirmation with staff mention at the end
+    await ctx.send(f"Donation Logged! User `{user}` donated `{amount_str}` OSRS gold. Logged by {staff_mention}")
+
+# Command that logs player payouts
+@slash_command(name="payout", description="Log a payout")
+@slash_option(      
+    name="user",
+    description="User this transaction is for",
+    required=True,
+    opt_type=OptionType.USER
+)
+@slash_option(
+    name="amount",
+    description="Amount of OSRS gold. Same logic as in game",
+    required=True,
+    opt_type=OptionType.STRING
+)
+async def payout_command(ctx: SlashContext, user: str, amount: str):
+    await ctx.defer()
+    amount_str = amount
+    amount = amount.replace(",", "")
+    
+    # Checks that the input amount is valid format
+    if not re.match(AMOUNT_PATTERN, amount):
+        await ctx.send(f"Amount given ({amount}) is not valid. Please follow same logic in game for typing amounts (eg. 42244, 24m, 11k)")
+        return
+
+    type = "payout"
+    amount = parse_amount(amount)
+    staff = ctx.author_id
+
+    insert_transaction(type, user, staff, amount)# Fetch the staff member's username for mention
+    
+    staff_member = await ctx.bot.fetch_user(staff)
+    staff_mention = f"<@{staff_member.id}>"  # Mention staff
+
+    # Send confirmation with staff mention at the end
+    await ctx.send(f"Payout Logged! User `{user}` donated `{amount_str}` OSRS gold. Logged by {staff_mention}")
+
+
+# Creates bot object
+bot = Client(intents=Intents.DEFAULT)
+
+@listen()
+async def on_ready():
+    print("Ready")
+    print(f"This bot is owned by {bot.owner}")
+
+# Starts bot
 bot.start(os.getenv("BOT_TOKEN"))
